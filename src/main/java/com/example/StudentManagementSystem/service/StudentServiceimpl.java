f20e5b2b86232a653457ad730dfef58fa779a1b2
Amalsha47 <bandara-ps20048@stu.kln.ac.lk>
2024-11-15 14:06:15 +0530

    first commit

---
+++ src/main/java/com/example/StudentManagementSystem/service/StudentServiceimpl.java
@@ -0,0 +1,58 @@
+package com.example.StudentManagementSystem.service;
+import com.example.StudentManagementSystem.model.Student;
+import com.example.StudentManagementSystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class StudentServiceimpl implements StudentService {
+ @Autowired
+ private StudentRepository studentRepository;
+ //save student in database
+ @Override
+ public Student saveStudent(Student student){
+ return studentRepository.save(student);
+ }
+ //get all student form database
+ @Override
+ public List<Student> getAllStudent() {
+ return studentRepository.findAll();
+ }
+ //get student using id
+ @Override
+ public Student getStudentById(long id) {
+ Optional<Student> student = studentRepository.findById(id);
+ if(student.isPresent()){
+ return student.get();
+ }else {
+ throw new RuntimeException();
+ }
+ }
+ //update student
+ @Override
+ public Student updateStudent(Student student, long id) {
+ Student existingStudent = 
+studentRepository.findById(id).orElseThrow(
+ ()-> new RuntimeException()
+ );
+ existingStudent.setFirstName(student.getFirstName());
+ existingStudent.setLastName(student.getLastName());
+ existingStudent.setEmail(student.getEmail());
+ existingStudent.setDepartment(student.getDepartment());
+ existingStudent.setYearOfEnrollment(student.getYearOfEnrollment());
+ // save
+ studentRepository.save(existingStudent);
+ return existingStudent;
+ }
+ //delete student
+ @Override
+ public void deleteStudent(long id) {
+ //check
+ studentRepository.findById(id).orElseThrow(()-> new 
+RuntimeException());
+ //delete
+ studentRepository.deleteById(id);
+ }
+
+}
