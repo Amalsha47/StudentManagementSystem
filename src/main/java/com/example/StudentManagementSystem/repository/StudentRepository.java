6440a2653c5325d140af513ecf02f548819e24a2
Amalsha47 <bandara-ps20048@stu.kln.ac.lk>
2024-11-15 14:36:05 +0530

    changed code

--- src/main/java/com/example/StudentManagementSystem/repository/StudentRepository.java
+++ src/main/java/com/example/StudentManagementSystem/repository/StudentRepository.java
@@ -1,6 +1,8 @@
 package com.example.StudentManagementSystem.repository;
 import com.example.StudentManagementSystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 public interface StudentRepository extends JpaRepository<Student,Long> {
  //crud
+ List<Student> findByYearOfEnrollment(int YearOfEnrollment);
 }
